e723fd169fcf89d19302502894c4878961cda164
Facundo Aranda <facundor.aranda@gmail.com>
2024-07-06 23:20:14 -0300

    Todavia esta incompleto, no me corrijas todavia por favor!

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDRs1el0nNYX937qYnMGqy0bs5lvqd4bfI",
+  authDomain: "react-coderhouse-d04e2.firebaseapp.com",
+  projectId: "react-coderhouse-d04e2",
+  storageBucket: "react-coderhouse-d04e2.appspot.com",
+  messagingSenderId: "396102168609",
+  appId: "1:396102168609:web:065fe57ad561a27a6db360",
+  measurementId: "G-C0CNH7CMX6"
+};
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from './firebase/config.js';
 import './index.css'
+
+
+
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
